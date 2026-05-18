c04d49e6fc2fa0cab4b67a9fde001b947a20b7c4
Pierre Marion <148580144+pierre-marion@users.noreply.github.com>
2026-05-18 15:46:48 +0200

    style(competitions): fond crème (#EFE9DC) derrière les emblèmes au lieu du blanc pur

--- components/CompetitionCard.tsx
+++ components/CompetitionCard.tsx
@@ -254,7 +254,7 @@ const styles = StyleSheet.create({
     width: 36,
     height: 36,
     borderRadius: 8,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#EFE9DC',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 4,
